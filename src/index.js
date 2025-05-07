e93f2430ec0a2a2880a01cb4bb7231ced063f5f1
eastID-10 <eastidmyt@gmail.com>
2025-05-07 13:23:59 +0700

    custom

--- src/index.js
+++ src/index.js
@@ -19,8 +19,8 @@ app.use((req, res, next) => {
 
 app.use(errorMiddleware);
 
-// app.listen(3000, () => {
-//   console.log("Server is running on port 3000");
-// });
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 export default app;
